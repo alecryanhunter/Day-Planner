7c3e25774e1d637c3918eb80b0d05356709cbb1d
Alec Hunter <alecryanhunter@gmail.com>
2023-04-06 11:28:31 -0700

    now pulls from local storage and fills hour slots

--- script.js
+++ script.js
@@ -52,7 +52,11 @@ $(document).ready($(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-
+  var hours = ["hour-9","hour-10","hour-11"]
+  for (i=0;i<hours.length;i++) {
+    var stored = localStorage.getItem(hours[i])
+    $("#"+hours[i]).children("textarea").val(stored);
+  }
   
   // Displays the current date in the header of the page.
   $("#currentDay").text(dayjs().format("dddd, MMMM D"))
