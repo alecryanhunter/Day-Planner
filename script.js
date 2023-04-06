855ef4ad6d27b7676df4482f2aa53fa6ad49afce
Alec Hunter <alecryanhunter@gmail.com>
2023-04-06 13:27:45 -0700

    consolidated code

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+// This function holds all the scripting for the page and waits until the page
+// has loaded to execute any of it.
 $(document).ready($(function () {
 
-  // This trims the whitespace, but still allows whitespace to be entered, as a
-  // delete function.
+  // When clicking on any save button, it pulls the value of sibling textarea
+  // via DOM traversal, trims the whitespace and stores it in Local Storage.
+  // It uses the id on the parent div as the storage key.
   $(".saveBtn").on("click",function(){
     var hourIndex = $(this).parent()[0].id;
     var eventsText = $(this).siblings("textarea")[0].value.trim();
@@ -10,10 +13,10 @@ $(document).ready($(function () {
   
   // Coerces the current time into a number for arithmetic purposes
   var currentHour = Number(dayjs().format("HH"));
-  var hourSlots = $(".container-lg").children()
+  var hourSlots = $(".container-lg").children();
   for (i=0;i<hourSlots.length;i++) {
-    var hourId = hourSlots[i].id
-    var hour = Number(hourId.slice(hourId.search("-")+1))
+    var hourId = hourSlots[i].id;
+    var hour = Number(hourId.slice(hourId.search("-")+1));
     if (currentHour<=hour) {
       if (hour===currentHour) {
         $(hourSlots[i]).addClass("present")
@@ -23,13 +26,11 @@ $(document).ready($(function () {
     } else {
       $(hourSlots[i]).addClass("past")
     }
-  }
-
-  // This function loops over the time blocks on the page and replaces whatever
-  // text is there with the text from Local Storage.
-  for (i=0;i<hourSlots.length;i++) {
-    var stored = localStorage.getItem(hourSlots[i].id)
-    $("#"+hourSlots[i].id).children("textarea").val(stored);
+    
+    // This section of the for loop pulls the stored values from Local Storage
+    // and puts them in their relevant text areas.
+    var stored = localStorage.getItem(hourId)
+    $("#"+hourId).children("textarea").val(stored);
   }
   
   // Displays the current date in the header of the page.
